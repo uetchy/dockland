f798842c4a4515eaae20da9e63256c80faab6257
Yasuaki Uechi <uetchy@randompaper.co>
2014-05-30 15:25:07 +0900

    Dockland detects automatically that given command requires <app_name>

--- lib/dockland.rb
+++ lib/dockland.rb
@@ -4,15 +4,27 @@ require 'dockland/cli'
 module Dockland
 
   private
-  def self.dokku_remote
-    dokku_uri = `git config remote.dokku.url`.strip
+  def self.parse_git_remote
+    dokku_uri = `git config remote.dokku.url`.strip || `git config remote.deploy.url`.strip
     if dokku_uri.empty?
-      puts "dokku url not given. please add dokku url to git remote"
+      puts "dokku url not given. please run > $ git remote add dokku dokku@<host>:<app_name>"
       exit
     end
 
     username, host, app_name = dokku_uri.scan(/\A(.+?)@(.+?):(.+?)\z/).first
     return { username: username, host: host, app_name: app_name }
+  end
+
+  def self.is_require_app_name?(command)
+    remote = parse_git_remote
+    line = `ssh -t #{remote[:username]}@#{remote[:host]} help 2> /dev/null | grep '    #{command}'`
+    line.include?('<app>')
+  end
+
+  def self.dokku_exec(command, options='')
+    remote = parse_git_remote
+    lines = `ssh -t #{remote[:username]}@#{remote[:host]} #{command} #{remote[:app_name] if is_require_app_name?(command)} #{options} 2> /dev/null`
+    lines.strip
   end
 
 end
